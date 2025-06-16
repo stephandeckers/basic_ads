d2368f539764497acbac4c483f542fd2534f13c7
Stephan Deckers <stephan.deckers@evesto.com>
2025-06-16 19:28:16 +0200

    20250616-19:28

--- app/src/main/java/com/example/basicads/MainActivity.java
+++ app/src/main/java/com/example/basicads/MainActivity.java
@@ -1,22 +1,31 @@
 package com.example.basicads;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.LoadAdError;
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.admanager.AdManagerAdRequest;
 import com.google.android.gms.ads.admanager.AdManagerAdView;
+import com.google.android.gms.ads.interstitial.InterstitialAd;
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
 
 public class MainActivity extends AppCompatActivity {
 
     private FrameLayout adContainerView; // or ViewGroup
+    private InterstitialAd interstitialAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +57,41 @@ public class MainActivity extends AppCompatActivity {
 
         adView.loadAd(new AdManagerAdRequest.Builder().build());
 
+        // Find the button by its ID
+        Button button = findViewById(R.id.button);
+
+        // Set click listener
+        button.setOnClickListener(view -> {
+            // Code to execute when button is clicked
+            Toast.makeText(MainActivity.this, "Button clicked!", Toast.LENGTH_SHORT).show();
+
+            // You can add any other actions here
+            // For example:
+            // startActivity(new Intent(this, AnotherActivity.class));
+
+            // beof add
+
+            // --- werkt niet goed (20250616 SDE)
+            
+            InterstitialAd.load(
+                    this,
+                    "ca-app-pub-5718236948049728/3498283882",
+                    new AdRequest.Builder().build(),
+                    new InterstitialAdLoadCallback() {
+                        @Override
+                        public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
+                            Log.d("BasicAds", "Ad was loaded.");
+                            MainActivity.this.interstitialAd = interstitialAd;
+                        }
+
+                        @Override
+                        public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                            Log.d("BasicAds", loadAdError.getMessage());
+                            interstitialAd = null;
+                        }
+                    });
+            // eof ad
+        });
         //setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
